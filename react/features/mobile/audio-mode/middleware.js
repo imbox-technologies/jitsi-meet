93f77e0d1996d2c2f4118ffe98f0e2dd610fdf35
Alvaro Marcos <alvaro.marcos@imbox.me>
2021-09-27 17:55:02 +0200

    Audio modes for iOS

--- react/features/mobile/audio-mode/middleware.js
+++ react/features/mobile/audio-mode/middleware.js
@@ -9,7 +9,7 @@ import {
     CONFERENCE_JOINED,
     getCurrentConference
 } from '../../base/conference';
-import { getFeatureFlag, AUDIO_FOCUS_DISABLED } from '../../base/flags';
+import { getFeatureFlag, AUDIO_FOCUS_DISABLED, CALL_INTEGRATION_ENABLED } from '../../base/flags';
 import { MiddlewareRegistry } from '../../base/redux';
 
 import { _SET_AUDIOMODE_DEVICES, _SET_AUDIOMODE_SUBSCRIPTIONS } from './actionTypes';
@@ -138,12 +138,13 @@ function _updateAudioMode({ getState }, next, action) {
     const state = getState();
     const conference = getCurrentConference(state);
     const { enabled: audioOnly } = state['features/base/audio-only'];
+    const callIntegration = getFeatureFlag(state, CALL_INTEGRATION_ENABLED, false);
     let mode;
 
     if (getFeatureFlag(state, AUDIO_FOCUS_DISABLED, false)) {
         return result;
     } else if (conference) {
-        mode = audioOnly ? AudioMode.AUDIO_CALL : AudioMode.VIDEO_CALL;
+        mode = audioOnly || callIntegration ? AudioMode.AUDIO_CALL : AudioMode.VIDEO_CALL;
     } else {
         mode = AudioMode.DEFAULT;
     }
